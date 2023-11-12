e02d3c5495a2febbb7642f34d5030ce173af5bb1
Stephan Teodosescu <32054537+steodose@users.noreply.github.com>
2023-11-12 11:54:26 -0800

    Update to remove seaborn package dependency

--- odds_converter.py
+++ odds_converter.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd 
 import numpy as np
 #import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 from fractions import Fraction
 from streamlit_extras.metric_cards import style_metric_cards
 from streamlit_extras.badges import badge
