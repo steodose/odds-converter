c90ff42c0260b082e0dda6cc73cfd185bfdf4f14
Stephan Teodosescu <32054537+steodose@users.noreply.github.com>
2023-11-12 19:39:40 -0800

    Updating odds csv url

--- odds_converter.py
+++ odds_converter.py
@@ -105,7 +105,7 @@ tab1, tab2 = st.tabs(["📈 Odds", "🗃 About"])
 
 @st.cache_data #used to be st.cache I believe
 def load_data():
-    df = pd.read_csv("/Users/Stephan/Desktop/Python/Odds-Converter/odds.csv")
+    df = pd.read_csv('https://raw.githubusercontent.com/steodose/odds-converter/main/odds.csv')
     return df
 
 df = load_data()
